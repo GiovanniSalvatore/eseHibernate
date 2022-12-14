2c7821d102006070b450b54bbd324ed06aadfa79
Giovanni <giosal21@gmail.com>
2022-12-14 12:38:04 +0100

    eseHibernate

---
+++ src/main/java/co/develhope/hybernate/entities/Enrollment.java
@@ -0,0 +1,22 @@
+package co.develhope.hybernate.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+public class Enrollment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Student student;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private SchoolClass classes;
+}
---
+++ src/main/java/co/develhope/hybernate/entities/Student.java
@@ -0,0 +1,25 @@
+package co.develhope.hybernate.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(unique = true, nullable = false)
+    private String email;
+
+}
